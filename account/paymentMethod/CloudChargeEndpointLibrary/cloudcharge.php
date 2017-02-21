f146414e25c3d496e622b568afc8e5a71c65e74e
gunathilakaishara <gunathilakaishara@hotmail.com>
2017-02-21 11:09:05 +0530

    changed

--- account/paymentMethod/CloudChargeEndpointLibrary/cloudcharge.php
+++ account/paymentMethod/CloudChargeEndpointLibrary/cloudcharge.php
@@ -19,7 +19,7 @@
 
 
 ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
+ini_set('display_startup_errors', 0);
 error_reporting(E_ALL);
 
 require_once($_SERVER['DOCUMENT_ROOT'] ."/dwcommon.php");
