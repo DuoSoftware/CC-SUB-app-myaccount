aad3cc83ca98d31100665a2eb129535e006a8d7d
Kasun <kasun.w@duosoftware.com>
2017-03-30 10:54:02 +0530

    Version 6.1.0.10

--- account/account.module.js
+++ account/account.module.js
@@ -1,9 +1,9 @@
 ////////////////////////////////
 // App : MyAccount / MyProfile
 // Owner : Ishara Gunathilaka
-// Last changed date : 2017/03/22
-// Version : 6.1.0.9
-// Modified By : Ishara
+// Last changed date : 2017/03/30
+// Version : 6.1.0.10
+// Modified By : Kasun
 /////////////////////////////////
 
 
