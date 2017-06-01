512d6642db09671d8d0d58d2306be4240f76e7c8
buddhika <buddhika@duosoftware.com>
2017-06-01 09:41:54 +0000

    removed app URL

--- account/paymentMethod/payment-partial.php
+++ account/paymentMethod/payment-partial.php
@@ -7,7 +7,7 @@ require_once ($doc.'/services/config/settings.php');
 
  define('CLIENT_ID', 'ca_9PpA3YTuMERCqYWgdj2ORagy9THaCOVO');
  //define('TOKEN_URI', ''. MAIN_DOMAIN .'/services/duosoftware.paymentgateway.service/stripe/insertAccKeys');
- define('TOKEN_URI', 'http://app.cloudcharge.com/services/duosoftware.paymentgateway.service/stripe/insertAccKeys');
+ define('TOKEN_URI', 'http://cloudcharge.com/services/duosoftware.paymentgateway.service/stripe/insertAccKeys');
  define('AUTHORIZE_URI', 'https://connect.stripe.com/oauth/authorize');
    if (isset($_GET['code'])) {
      $code = $_GET['code'];
@@ -74,7 +74,7 @@ require_once ($doc.'/services/config/settings.php');
        'client_id' => CLIENT_ID,
        'state' => $_SERVER['SERVER_NAME'].'@'.$_COOKIE['securityToken']
        //,'redirect_uri'=> MAIN_DOMAIN .'/azureshell/app/main/account/paymentMethod/payment-partial.php'
-       ,'redirect_uri'=> 'http://app.cloudcharge.com/azureshell/app/main/account/paymentMethod/payment-partial.php'
+       ,'redirect_uri'=> 'http://cloudcharge.com/azureshell/app/main/account/paymentMethod/payment-partial.php'
      );
 
    $url = AUTHORIZE_URI . '?' . http_build_query($authorize_request_body);
