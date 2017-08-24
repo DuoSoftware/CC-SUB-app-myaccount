abe55f721e87fc5dd3363331d701faeeedfda4a6
Kasun <kasun.w@duosoftware.com>
2017-08-24 16:15:37 +0530

    Version 6.1.0.24

--- account/account.module.js
+++ account/account.module.js
@@ -1,9 +1,9 @@
 ////////////////////////////////
 // App : MyAccount / MyProfile
 // Owner : Ishara Gunathilaka
-// Last changed date : 2017/08/23
-// Version : 6.1.0.21
-// Modified By : Ishara
+// Last changed date : 2017/08/24
+// Version : 6.1.0.22
+// Modified By : Kasun
 /////////////////////////////////
 
 
@@ -20,6 +20,21 @@
   {
 
     // State
+    function gst(name) {
+        var nameEQ = name + "=";
+        var ca = document.cookie.split(';');
+        for (var i = 0; i < ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+            if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+        }
+        //debugger;
+        return null;
+    }
+    /** Check for Super admin */
+    var isSuperAdmin = gst('isSuperAdmin');
+    /** Check for Super admin - END */
+
     $stateProvider
       .state('app.account', {
         url    : '/account',
@@ -30,18 +45,32 @@
           }
         },
         resolve: {
+          security: ['$q','mesentitlement','$timeout','$location', function($q,mesentitlement,$timeout, $location){
+            return $q(function(resolve, reject) {
+              $timeout(function() {
+                if (isSuperAdmin != 'true') {
+                  resolve(function () {
+                    var entitledStatesReturn = mesentitlement.stateDepResolver('account');
+
+                    mesentitlementProvider.setStateCheck("account");
+                  });
+                }
+              });
+            });
+          }]
 
         },
         bodyClass: 'account'
       });
 
-    msNavigationServiceProvider.saveItem('account', {
-       // title    : 'My Account',
-      state    : 'app.account',
-      /*stateParams: {
-       'param1': 'page'
-       },*/
-      weight   : 1
-    });
-  }
+      if(isSuperAdmin != 'true'){
+        msNavigationServiceProvider.saveItem('account', {
+           // title    : 'My Account',
+          state    : 'app.account',
+          /*stateParams: {
+           'param1': 'page'
+           },*/
+          weight   : 1
+        });
+      }
 })();
