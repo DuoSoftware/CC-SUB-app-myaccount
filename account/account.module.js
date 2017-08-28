b6c941cee58b0f9da57a50235d615885ec68f99d
Kasun <kasun.w@duosoftware.com>
2017-08-28 17:17:22 +0530

    Revert test code

--- account/account.module.js
+++ account/account.module.js
@@ -2,78 +2,56 @@
 // App : MyAccount / MyProfile
 // Owner : Ishara Gunathilaka
 // Last changed date : 2017/08/24
-// Version : 6.1.0.22
+// Version : 6.1.0.21
 // Modified By : Kasun
 /////////////////////////////////
 
 
 (function ()
 {
-  'use strict';
+	'use strict';
 
-  angular
-    .module('app.account',['stripe-payment-tools'])
-    .config(config);
+	angular
+		.module('app.account',['stripe-payment-tools'])
+		.config(config);
 
-  /** @ngInject */
-  function config($stateProvider, $translatePartialLoaderProvider, msApiProvider, msNavigationServiceProvider)
-  {
+	/** @ngInject */
+	function config($stateProvider, $translatePartialLoaderProvider, msApiProvider, msNavigationServiceProvider)
+	{
 
-    // State
-    function gst(name) {
-        var nameEQ = name + "=";
-        var ca = document.cookie.split(';');
-        for (var i = 0; i < ca.length; i++) {
-            var c = ca[i];
-            while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-            if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
-        }
-        //debugger;
-        return null;
-    }
-    /** Check for Super admin */
-    var isSuperAdmin = gst('isSuperAdmin');
-    /** Check for Super admin - END */
+		$stateProvider
+			.state('app.account', {
+				url    : '/account',
+				views  : {
+					'account@app': {
+						templateUrl: 'app/main/account/account.html',
+						controller : 'AccountController as vm'
+					}
+				},
+				resolve: {
+					security: ['$q','mesentitlement','$timeout','$location', function($q,mesentitlement,$timeout, $location){
+						return $q(function(resolve, reject) {
+							$timeout(function() {
+								resolve(function () {
+									var entitledStatesReturn = mesentitlement.stateDepResolver('account');
 
-    $stateProvider
-      .state('app.account', {
-        url    : '/account',
-        views  : {
-          'account@app': {
-            templateUrl: 'app/main/account/account.html',
-            controller : 'AccountController as vm'
-          }
-        },
-        resolve: {
-          security: ['$q','mesentitlement','$timeout','$location', function($q,mesentitlement,$timeout, $location){
-            return $q(function(resolve, reject) {
-              $timeout(function() {
-                if (isSuperAdmin != 'true') {
-                  resolve(function () {
-                    var entitledStatesReturn = mesentitlement.stateDepResolver('account');
+									mesentitlementProvider.setStateCheck("account");
+								});
+							});
+						});
+					}]
 
-                    mesentitlementProvider.setStateCheck("account");
-                  });
-                }
-              });
-            });
-          }]
+				},
+				bodyClass: 'account'
+			});
 
-        },
-        bodyClass: 'account'
-      });
-
-    if(isSuperAdmin != 'true'){
-      msNavigationServiceProvider.saveItem('account', {
-         // title    : 'My Account',
-        state    : 'app.account',
-        /*stateParams: {
-         'param1': 'page'
-         },*/
-        weight   : 1
-      });
-    }
-  }
-
-      
+		msNavigationServiceProvider.saveItem('account', {
+			// title    : 'My Account',
+			state    : 'app.account',
+			/*stateParams: {
+			 'param1': 'page'
+			 },*/
+			weight   : 1
+		});
+	}
 })();
